c872fd22e9c416ac9b3a8bd302c0efefdcdd0a6d
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-08-17 22:09:45 -0400

    Added html site base

---
+++ src/net/arptell/dhcpcord/exceptions/PortBindException.java
@@ -0,0 +1,8 @@
+package net.arptell.dhcpcord.exceptions;
+
+public class PortBindException extends Exception{
+
+	public PortBindException(String reason) {
+		super(reason);
+	}
+}
---
+++ src/net/arptell/dhcpcord/handlers/TCPConnectionHandler.java
@@ -0,0 +1,6 @@
+package net.arptell.dhcpcord.handlers;
+
+public class TCPConnectionHandler {
+
+	//Will be used in the future
+}
